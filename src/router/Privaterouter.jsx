e0dcf9486068f3578d8b6a963568c761adecc3b0
mustafa3306 <mustafaworkkok@gmail.com>
2022-07-14 14:06:02 +0300

    file

--- src/router/Privaterouter.jsx
+++ src/router/Privaterouter.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom';
 
-const Privaterouter = () => {
-  return (
-    <div>Privaterouter</div>
-  )
-}
+const PrivateRouter = () => {
+  const user = JSON.parse(sessionStorage.getItem('user'));
 
-export default Privaterouter
+  return user ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRouter;
